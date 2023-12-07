2f41ca8b1637d5cca4627323dc0870af44508110
SergioScata <sergiopio395@gmail.com>
2023-12-07 14:37:59 +0100

    Refactoring

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,43 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("quanti prodotti devi inserire?");
+        int numberOfProduct = Integer.parseInt(scan.nextLine());
+
+        Prodotto[] products = new Prodotto[numberOfProduct];
+
+
+        try {
+            for (int i=0;i<products.length;i++) {
+                System.out.println("Inserisci il nome del prodotto");
+                String name = scan.nextLine();
+                System.out.println("Inserisci una descrizione del prodotto");
+                String description = scan.nextLine();
+                System.out.println("Inserisci il costo del prodotto");
+                double price = Double.parseDouble(scan.nextLine());
+                System.out.println("Inserisci l'iva: ");
+                int iva = Integer.parseInt(scan.nextLine());
+                Prodotto product = new Prodotto(name, description, price, iva);
+                products[i] = product;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("You put an invalid data" + e.getMessage());
+        }
+
+
+        System.out.println("i tuoi prodotti");
+        for (int i=0; i<products.length;i++){
+            System.out.println("prodotto:"+ products[i].getName()+" " + products[i].getCode() +" "+ "descrizione:"+ " "+ products[i].getDescription()+ " " + "prezzo"+" "+ products[i].getPrice()+"€"+ " " + "prezzo con iva:"+ products[i].priceIva()+"€"+ " " + products[i].nameCode());
+        }
+
+
+
+
+        scan.close();
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,2 +1,80 @@
-package org.lessons.java.shop;public class Prodotto {
+ package org.lessons.java.shop;
+
+import java.util.Random;
+
+    public class Prodotto {
+
+        //ATTRIBUTI
+        private int code;
+        private String name;
+        private String description;
+        private double price;
+        private double iva;
+
+        //COSTRUTTORI
+
+        public Prodotto( String name, String description, double price, int iva)throws IllegalArgumentException {
+           if(name == null){throw new IllegalArgumentException("Insert a valid name");}
+            if(price< 0){throw new IllegalArgumentException("Insert a valid name");}
+            if(iva< 0){throw new IllegalArgumentException("Insert a valid name");}
+            this.code = code;
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.iva = iva;
+        }
+
+
+        //GET AND SET
+
+        public int getCode() {
+            int codeProduct = randomCode.nextInt(1000000,99999999);
+            return codeProduct;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public double getIva() {
+            return iva;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public void setIva(double iva) {
+            this.iva = iva;
+        }
+
+        //MODULI
+        Random randomCode = new Random();
+
+        private int codeProduct = randomCode.nextInt(10000,99999);
+
+        public double priceIva(){
+             return price + (price * iva / 100);
+        }
+
+        public String nameCode (){
+            return name + codeProduct;
+        }
+
 }
